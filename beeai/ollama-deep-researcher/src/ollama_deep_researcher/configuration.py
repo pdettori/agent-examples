1f8851e07c8e3458d804b846f5466e1db412c367
Jarek Cwiklik <cwiklik@mac.lan>
2025-04-17 10:40:23 -0400

    initial beeai code
    Signed-off-by: Jarek Cwiklik <cwiklik@mac.lan>

---
+++ beeai/ollama-deep-researcher/src/ollama_deep_researcher/configuration.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from pydantic_settings import BaseSettings
+
+
+class SearchAPI(Enum):
+    PERPLEXITY = "perplexity"
+    TAVILY = "tavily"
+    DUCKDUCKGO = "duckduckgo"
+
+
+class Configuration(BaseSettings):
+    llm_model: str = "llama3.1"
+    llm_api_base: str = "http://localhost:11434/v1"
+    llm_api_key: str = "dummy"
+
+    max_web_research_loops: int = 3
+    search_api: SearchAPI = SearchAPI.DUCKDUCKGO
+    fetch_full_page: bool = False
