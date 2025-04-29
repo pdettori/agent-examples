9ad3f063ef30a942373849fee5281a73fb62cd3f
Paolo Dettori <dettori@us.ibm.com>
2025-04-28 21:12:57 -0400

    add dockerfile to a22/marvin example
    Signed-off-by: Paolo Dettori <dettori@us.ibm.com>

--- a2a/langgraph/__main__.py
+++ a2a/langgraph/__main__.py
@@ -33,8 +33,7 @@ def main(host, port):
         agent_card = AgentCard(
             name="Currency Agent",
             description="Helps with exchange rates for currencies",
-            #url=f"http://{host}:{port}/",
-            url=f"http://{host}:9000/",
+            url=f"http://{host}:{port}/",
             version="1.0.0",
             defaultInputModes=CurrencyAgent.SUPPORTED_CONTENT_TYPES,
             defaultOutputModes=CurrencyAgent.SUPPORTED_CONTENT_TYPES,
